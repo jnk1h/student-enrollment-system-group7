ab93a94bc1ea88f6b879c870cc28a8861594c03d
Allan-Kiprono <allan.kirui@strathmore.edu>
2025-08-24 20:21:36 +0300

    Added Permissions and Test Screenshots

--- enrollment/views.py
+++ enrollment/views.py
@@ -2,25 +2,31 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Department, Instructor, Course, Student, Enrollment
 from .serializers import DepartmentSerializer, InstructorSerializer, CourseSerializer, StudentSerializer, EnrollmentSerializer
+from rest_framework.permissions import DjangoModelPermissions
 
 # Create your viewsets here.
 class DepartmentViewSet (viewsets.ModelViewSet):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
+    permission_classes = [DjangoModelPermissions]
 
 class InstructorViewSet (viewsets.ModelViewSet):
     queryset = Instructor.objects.all()
     serializer_class = InstructorSerializer
+    permission_classes = [DjangoModelPermissions]
 
 class CourseViewSet (viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    permission_classes = [DjangoModelPermissions]
 
 
 class StudentViewSet (viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    permission_classes = [DjangoModelPermissions]
 
 class EnrollmentViewSet (viewsets.ModelViewSet):
     queryset = Enrollment.objects.all()
     serializer_class = EnrollmentSerializer
+    permission_classes = [DjangoModelPermissions]
